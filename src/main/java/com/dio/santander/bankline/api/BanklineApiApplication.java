f8fb9cc763b8d8613dd9bd85ccd66eb2971ff5c5
ValdirRoncato <104869163+ValdirRoncato@users.noreply.github.com>
2022-05-04 20:17:49 -0300

    nova atualização

---
+++ src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
@@ -0,0 +1,13 @@
+package com.dio.santander.bankline.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanklineApiApplication {
+//teste de atualização
+	public static void main(String[] args) {
+		SpringApplication.run(BanklineApiApplication.class, args);
+	}
+
+}
